e5010c36b097d712d44197f416afb71f2e112ca1
Shine-Light <3120815902@qq.com>
2022-06-12 12:39:12 +0800

    修复复读会重复命令的问题

--- content/plugins/repeater/__init__.py
+++ content/plugins/repeater/__init__.py
@@ -4,7 +4,7 @@
 @Date: 2022/5/31 20:49
 """
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, Message
-from nonebot import on_message
+from nonebot import on_message, get_driver
 from nonebot.exception import FinishedException
 from nonebot.log import logger
 
@@ -16,6 +16,7 @@ repeater = on_message(priority=12)
 async def _(event: GroupMessageEvent):
     if isinstance(event, GroupMessageEvent):
         try:
+            pres: set = get_driver().config.command_start
             # 只重复文字信息
             msg = event.get_plaintext()
             if not msg:
@@ -24,6 +25,11 @@ async def _(event: GroupMessageEvent):
             # 防止重复 CQ码+空格 信息
             if msg == " ":
                 if not str(event.get_message()) == " ":
+                    return
+
+            # 不重复命令
+            for pre in pres:
+                if pre in msg:
                     return
 
             gid = event.group_id
