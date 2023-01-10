9a51ca13e749ef0d03995809149ce20ecd90b617
Shine-Light <3120815902@qq.com>
2023-01-10 16:59:23 +0800

    修改复读逻辑,并支持触发次数配置

--- content/plugins/repeater/__init__.py
+++ content/plugins/repeater/__init__.py
@@ -9,83 +9,88 @@ from nonebot.exception import FinishedException
 from nonebot.log import logger
 from nonebot.plugin import PluginMetadata
 
-from utils.other import add_target, translate
+from utils.other import add_target
+from utils.config import manager
 
 
 # 插件元数据定义
 __plugin_meta__ = PluginMetadata(
-    name=translate("e2c", "repeater"),
+    name="repeater",
     description="自动+1",
-    usage="被动,无命令" + add_target(60)
+    usage="被动,无命令" + add_target(60),
+    extra={
+        "generate_type": "general",
+        "permission_common": "baned",
+        "unset": False,
+        "total_unable": True,
+        "version": "0.0.1",
+        "author": "Shine_Light",
+        "translate": "复读",
+        "configs_general": {
+          "count_max": 3  # 触发次数阈值
+        }
+    }
 )
 
 repeater_last = {}
 msg_last = {}
 
 
+def equals(msg1: Message, msg2: Message):
+    """
+    检查两个Message是否相等
+    """
+    flag = True
+    if len(msg1) != len(msg2):
+        return False
+    for i in range(len(msg1)):
+        if msg1[i].type != msg2[i].type:
+            flag = False
+            break
+        flag = msg1[i].data == msg2[i].data
+        if msg1[i].type == "image":
+            flag = msg1[i].data.get("file") == msg2[i].data.get("file")
+        if not flag:
+            break
+    return flag
+
+
 repeater = on_message(priority=12, block=False)
 @repeater.handle()
 async def _(event: GroupMessageEvent):
-    if isinstance(event, GroupMessageEvent):
-        try:
-            gid = event.group_id
-            pres: set = get_driver().config.command_start
+    try:
+        gid = event.group_id
+        pres: set = get_driver().config.command_start
+        count_max = manager.getPluginConfig("repeater").get_config_general("count_max")
+        msg = event.get_message()
+        # 空消息
+        if not (msg or len(msg)):
+            return
 
-            msg = str(event.get_message())
-            # 空消息
-            if not msg:
+        if not msg_last.get(gid):
+            msg_last.update({gid: {"msg": msg, "count": 1}})
+            return
+
+        # 不重复命令
+        for pre in pres:
+            if pre == msg[:len(pre)]:
+                msg_last.update({gid: {"msg": msg, "count": 1}})
                 return
 
-            if "[CQ" in msg:
-                if "[CQ:face" in msg or "[CQ:at" in msg or "[CQ:image" in msg:
-                    pass
-                else:
-                    try:
-                        msg_last.pop(gid)
-                        return
-                    except:
-                        return
+        # 只触发一次+1
+        if repeater_last.get(gid) and equals(msg, repeater_last.get(gid)):
+            return
 
-            # 只重复一个 +1 事件
-            try:
-                if repeater_last[gid] == msg:
-                    return
-                if "[CQ:image" in msg and "[CQ:image" in repeater_last[gid]:
-                    if repeater_last[gid].split("file=")[1].split(",")[0] == msg.split("file=")[1].split(",")[0]:
-                        return
-            except KeyError:
-                pass
+        count = msg_last.get(gid).get('count') + 1
 
-            # 不重复命令
-            for pre in pres:
-                if pre == msg[:len(pre)]:
-                    try:
-                        msg_last.pop(gid)
-                        return
-                    except:
-                        return
+        if count >= count_max and equals(msg, msg_last.get(gid).get('msg')):
+            await repeater.send(msg)
+            repeater_last.update({gid: msg})
+        elif repeater_last.get(gid):
+            repeater_last.pop(gid)
 
-            try:
-                if "[CQ:image" not in msg and gid in msg_last and msg == msg_last[gid]:
-                    msg_last.pop(gid)
-                    repeater_last[gid] = msg
-                    await repeater.finish(Message(msg))
-                elif "[CQ:image" in msg and gid in msg_last and "[CQ:image" in msg_last[gid]:
-                    if msg.split("file=")[1].split(",")[0] == msg_last[gid].split("file=")[1].split(",")[0]:
-                        msg_last.pop(gid)
-                        repeater_last[gid] = msg
-                        await repeater.finish(Message(msg))
-                    else:
-                        msg_last[gid] = msg
-                elif repeater_last[gid] != "":
-                    msg_last[gid] = msg
-                    repeater_last[gid] = ""
-                else:
-                    msg_last[gid] = msg
-            except KeyError:
-                msg_last[gid] = msg
-
-        except FinishedException:
-            pass
-        except Exception as e:
-            logger.error(e.__traceback__)
+        msg_last.update({gid: {'msg': msg, 'count': count}})
+    except FinishedException:
+        pass
+    except Exception as e:
+        logger.error(e.__traceback__)
