634f7ee94391e1f1ee14574866d2ca7f350ce287
Shine-Light <3120815902@qq.com>
2023-02-24 21:51:55 +0800

    原神祈愿支持3.4

--- content/plugins/GenshinPray/template/arm_up/spider/down.py
+++ content/plugins/GenshinPray/template/arm_up/spider/down.py
@@ -1,4 +1,5 @@
-import requests
+import aiohttp
+import asyncio
 import os
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -10,7 +11,21 @@ links = open("in_img.txt", "r", encoding="utf-8")
 name = open("in_name.txt", "r", encoding="utf-8").read().split("\n")
 a = 0
 
+
+async def download(url):
+    async with aiohttp.ClientSession() as session:
+        resp = await session.get(url=url, headers=headers)
+        return await resp.read()
+
+
+async def save(url, name_now):
+    content = await download(url)
+    with open(f"../img/{name_now}.png", "wb+") as img:
+        img.write(content)
+
+
+tasks = []
 for link in links:
-    with open(f"../img/{name[a]}.png", "wb+") as img:
-        img.write(requests.get(url=link, headers=headers).content)
+    tasks.append(save(link, name[a]))
     a += 1
+asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
--- content/plugins/GenshinPray/template/role_up/down.py
+++ content/plugins/GenshinPray/template/role_up/down.py
@@ -3,17 +3,33 @@
 @Version: 1.0
 @Date: 2022/8/17 22:16
 """
-import requests
+import aiohttp
+import asyncio
 import os
 
 path = os.path.abspath(__file__).replace("down.py", "")
 os.chdir(path)
 names_role = open("img/avatar/name.txt", "r", encoding="utf-8").read().strip().split("\n")
 # names_ele = open("img/ele/name.txt", "r", encoding="utf-8").read().strip().split("\n")
+url_base = "http://cdn.shinelight.xyz/nonebot/resource/GenshinPray/role_up/avatar/"
 
+
+async def download(url):
+    async with aiohttp.ClientSession() as session:
+        resp = await session.get(url=url)
+        return await resp.read()
+
+
+async def save(name_now):
+    content = await download(url_base + name_now)
+    with open(f"img/avatar/{name_now}", "wb+") as img:
+        img.write(content)
+
+
+tasks = []
 for name in names_role:
-    with open(f"img/avatar/{name}", "wb+") as img:
-        img.write(requests.get(url=f"http://cdn.shinelight.xyz/nonebot/resource/GenshinPray/role_up/avatar/{name}").content)
+    tasks.append(save(name))
+asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
 
 # for name in names_ele:
 #     with open(f"img/ele/{name}", "wb+") as img:
